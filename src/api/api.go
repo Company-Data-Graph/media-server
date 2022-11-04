450b8ee5cbe945ea0f8a13b7a5375c32da56237c
AnatoliyK <anatoliy.kizyulya@gmail.com>
2022-11-04 23:07:35 +0300

    feat: add helm and storage resources

--- src/api/api.go
+++ src/api/api.go
@@ -60,12 +60,20 @@ func (api *MediaAPI) SetCorsHeaders(rw *http.ResponseWriter) {
 }
 
 func (api *MediaAPI) Run() {
+	// For K8S probes Ping path
+	http.HandleFunc(fmt.Sprintf("%s%s", api.prefix, "/ping"), api.Ping)
+	//Apps path
 	http.HandleFunc(fmt.Sprintf("%s%s", api.Prefix, api.Routes.DataRoute.Name), api.getDataByUrl)
 	http.HandleFunc(fmt.Sprintf("%s%s", api.Prefix, "/signin"), api.signIn)
 	http.HandleFunc(fmt.Sprintf("%s%s", api.Prefix, "/upload"), api.upload)
 	http.HandleFunc(fmt.Sprintf("%s%s", api.Prefix, "/delete"), api.delete)
 	log.Printf("Run server on %s:%d", api.Host, api.Port)
 	http.ListenAndServe(fmt.Sprintf("%s:%d", api.Host, api.Port), nil)
+}
+
+func (api *MediaAPI) Ping(rw http.ResponseWriter, r *http.Request) {
+	api.SetCorsHeaders(&rw)
+	fmt.Fprintln(rw, "pong")
 }
 
 func (api *MediaAPI) signIn(rw http.ResponseWriter, r *http.Request) {
